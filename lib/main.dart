9b188ca69096a61f05b9b4afb174ec32a51e87ea
Karan1402 <karanchauhan.student@gmail.com>
2023-02-15 18:22:18 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// @dart=2.9
 import 'package:flutter/material.dart';
 import 'package:flash_chat_flutter_2/screens/welcome_screen.dart';
 import 'package:flash_chat_flutter_2/screens/login_screen.dart';
